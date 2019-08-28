106986b0083df9d366512dac310722fe60fdacdd
israel <israel.bejarano.a@gmail.com>
2019-08-28 23:16:31 +0200

    post: crear usuario

--- routes/usuario.ts
+++ routes/usuario.ts
@@ -1,12 +1,30 @@
 import { Router, Request, Response} from 'express';
+import { Usuario } from '../models/usuario.model';
 
 const userRoutes = Router();
 
-userRoutes.get('/prueba', (req: Request, res: Response) => {
-    res.json({
-        ok: true,
-        mensaje: 'todo funciona ok'
+userRoutes.post('/create', (req: Request, res: Response) => {
+    const user = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar
+    };
+
+    // guardarlo en BBDD
+    Usuario.create(user).then(userDB => {
+        res.json({
+            ok: true,
+            user
+        });
+    }).catch(err => {
+        res.json({
+            ok: false,
+            err
+        });
     });
+
+
 });
 
 export default userRoutes;
