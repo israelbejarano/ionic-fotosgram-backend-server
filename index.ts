37ca97474516fdd9d30ac961f19760c2fb345588
israel <israel.bejarano.a@gmail.com>
2019-08-28 22:51:45 +0200

    conectar la BBDD

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import Server from './classes/server';
+import userRoutes from './routes/usuario';
+import mongoose from 'mongoose';
 
 const server = new Server();
 
+
+// rutas de mi app
+server.app.use('/user', userRoutes);
+
+// conectar BBDD
+mongoose.connect('mongodb://localhost:27017/fotosgram', {useNewUrlParser: true, useCreateIndex: true}, (err) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+});
 
 // levantar el servidor
 server.start();
