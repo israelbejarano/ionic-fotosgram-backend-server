a677d7a1d15aa2a553436995ee88d4003c62b68f
israel <israel.bejarano.a@gmail.com>
2019-08-30 23:11:57 +0200

    sistema de ficheros para img de usuarios

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import Server from './classes/server';
 import userRoutes from './routes/usuario';
+import postRoutes from './routes/post';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
 
 const server = new Server();
 
@@ -9,9 +11,13 @@ const server = new Server();
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
 
+// fileupload
+server.app.use(fileUpload({useTempFiles: true}));
+
 
 // rutas de mi app
 server.app.use('/user', userRoutes);
+server.app.use('/posts', postRoutes);
 
 // conectar BBDD
 mongoose.connect('mongodb://localhost:27017/fotosgram', {useNewUrlParser: true, useCreateIndex: true}, (err) => {
--- routes/usuario.ts
+++ routes/usuario.ts
@@ -103,5 +103,13 @@ userRoutes.post('/update', verificaToken, (req: any, res: Response) => {
     });
 });
 
+userRoutes.get('/', [verificaToken], (req: any, res: Response) => {
+    const usuario = req.usuario;
+    res.json({
+        ok: true,
+        usuario
+    });
+});
+
 
 export default userRoutes;
